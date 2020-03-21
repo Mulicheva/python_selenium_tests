81739c10ee0498e8e3c107c915dd0dc51233da7c
Katya Mulicheva <miokeit@gmail.com>
2020-03-21 18:21:40 +0400

    added negative checking

--- pages/locators.py
+++ pages/locators.py
@@ -16,5 +16,5 @@ class ProductPageLocators():
     BOOK_NAME_MSG = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
     BOOK_NAME = (By.CSS_SELECTOR, '#content_inner > article > div.row > div.col-sm-6.product_main > h1')
     BOOK_PRICE_MSG = (By.CSS_SELECTOR, '#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong')
-
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
     BASKET_VALUE = (By.CSS_SELECTOR, '#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color')
--- pages/product_page.py
+++ pages/product_page.py
@@ -25,3 +25,9 @@ class ProductPage(BasePage):
            assert book_price_msg.text==basket_value.text, "Wrong book price message"
         else:
             print('Book price message not found')
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_be_dissapear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, and should dissapear"
